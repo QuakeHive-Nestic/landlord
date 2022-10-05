44f345abf80011160dcf67f59aaaa1159b3695d0
Ibrahim Yakubu <ibrahimy437@gmail.com>
2022-10-06 00:57:50 +0100

    Add navitem component

---
+++ components/Navitem.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+const NavItem = ({ text, href, active }) => {
+  return (
+    <Link href={href}>
+      <div className="theme">
+        <a className={`nav__item ${active ? "active" : ""}`}>{text}</a>
+      </div>
+    </Link>
+  );
+};
+
+export default NavItem;
